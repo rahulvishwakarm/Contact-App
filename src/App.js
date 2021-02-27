557ea3ebe052d8ed90c2841c0b73715c5809ba54
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-02-27 11:27:54 +0530

    Header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from '../src/data/Header';
+import DisplayContianer from '../src/data/DisplayContianer';
+
+
+function App() {
+  return (
+    <div classNameName="App">
+        <Header/>
+        <DisplayContianer/>
+    </div>
+  );
+}
+
+export default App;
